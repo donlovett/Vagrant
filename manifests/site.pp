ecb1d3a1d455643df08e2663c4faf534cae527a1
Don Lovett <Don Lovett>
2016-03-20 12:59:16 -0600

    Fix nptd command from article https://docs.puppetlabs.com/guides/module_guides/bgtm.html

--- manifests/site.pp
+++ manifests/site.pp
@@ -32,7 +32,20 @@ notify => Service["apache2"],
 
 }
 
-class class { 'ntp':
-   servers =>
-   ['nist-time-server.eoni.com','nist1-lv.ustiming.org','ntp-nist.ldsbc.edu']
+class ntp::service inherits ntp {
+
+      if ! ($service_ensure in [ 'running', 'stopped' ]) {
+        fail('service_ensure parameter must be running or stopped')
+      }
+
+      if $service_manage == true {
+        service { 'ntp':
+          ensure     => $service_ensure,
+          enable     => $service_enable,
+          name       => $service_name,
+          hasstatus  => true,
+          hasrestart => true,
+        }
+      }
+
 }
